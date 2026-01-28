472b781fb778d484eee8c728e86de061de0ae430
Paulo <pauloeduarduoo10@gmail.com>
2026-01-28 18:38:53 -0300

    chore: enable CORS and fix backend dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,16 @@ import 'dotenv/config';
 import authRoutes from './routes/auth.routes.js';
 import profileRoutes from './routes/profile.routes.js';
 import router from './routes/tasks/task.routes.js';
-
+import cors from "cors";
 
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 // middleware global para JSON
 app.use(express.json());
