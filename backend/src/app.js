71e42318962a61c33e83fb41f169d663ffffd983
Paulo <pauloeduarduoo10@gmail.com>
2026-02-03 18:40:16 -0300

    cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,9 +9,7 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: "*",
 }));
 
 // middleware global para JSON
